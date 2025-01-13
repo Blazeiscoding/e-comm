5c8242a26bfac63b3f7816e4138bdef4234329b6
Nikhil Rathore <114566513+Blazeiscoding@users.noreply.github.com>
2025-01-13 22:50:18 +0530

    Add function

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,3 +1,7 @@
+import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 
@@ -6,3 +10,23 @@ const razorpay = new Razorpay({
     key_secret: process.env.RAZORPAY_KEY_SECRET!
 });
 
+export async function POST(request: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+        if(!session){
+            return NextResponse.json({error:"Unauthorized"},{status:401})
+        }
+        const {productId,variant} = await request.json();
+        await connectToDatabase();
+
+        //create razorpay order
+        const order = await razorpay.orders.create({
+            amount: variant.price * 100,
+            currency: "INR",
+            
+        })
+
+    } catch (error) {
+        
+    }
+}
