a20cee17c1f5568c803588821d599d2b465bfc66
Nikhil Rathore <114566513+Blazeiscoding@users.noreply.github.com>
2025-01-14 21:15:53 +0530

    Add razorpay route

--- app/api/webhook/razorpay/route.ts
+++ app/api/webhook/razorpay/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto"
+import { connectToDatabase } from "@/lib/db";
+import Order from "@/models/Order";
+import nodemailer from "nodemailer";
+
 export async function POST(req: NextRequest){
     try {
         const body = await req.text();
@@ -9,7 +13,47 @@ export async function POST(req: NextRequest){
         if(signature !== expectedSignature){
             return NextResponse.json({error:"Invalid signature"},{status:401})
         }
+
+        const event = JSON.parse(body);
+        await connectToDatabase();
+
+        if(event.event=== "payment.captured"){
+            const payment = event.payload.payment.entity;
+
+            const order = await Order.findOneAndUpdate(
+                { razorpayOrderId: payment.order_id},
+                {
+                    razorpyaPaymentId: payment.id,
+                    status: "completed",
+                }
+            ).populate([
+                {path:"productId", select:"name"},
+                {path:"userId",select:"email"}
+            ])
+
+            if(order){
+                const transporter = nodemailer.createTransport({
+                    service: "sandbox.smtp.mailtrap.io",
+                    port: 2525,
+                    auth: {
+                        user: "dbfbf9d5e4ff59",
+                        pass: "e7d53a9261d5a4",
+
+                },
+             } );
+             await transporter.sendMail({
+                 from:"XtNf9@example.com",
+                 to: order.userId.email,
+                 subject:"Order Completed",
+                 text:`Your order ${order.productId.name} has been successfully placed`,
+             })
+            }
+
+        }
+
+        return NextResponse.json({message:"success"},{status:200})
     } catch (error) {
-        
+        console.log(error);
+        return NextResponse.json({error:"Something went wrong"},{status:500})
     }
 }
