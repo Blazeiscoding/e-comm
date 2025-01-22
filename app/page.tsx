a1087b34cb44cdccfefca9d90674c681416b8e9e
Nikhil Rathore <114566513+Blazeiscoding@users.noreply.github.com>
2025-01-22 21:13:01 +0530

    Add mini-svg-data-uri package, enhance Tailwind CSS configuration, and implement scroll animation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,108 @@
+
 "use client";
 
-import React, { useEffect, useState } from "react";
-import ImageGallery from "./components/ImageGallery";
-import { IProduct } from "@/models/Product";
-import { apiClient } from "@/lib/api-client";
-
+import React from "react";
+import { HeroParallax } from "../components/ui/hero-parallax";
 export default function Home() {
-  
 
   return (
-    <main className="container mx-auto px-4 py-8">
-      
+    <main>
+      <HeroParallax products={products} />
     </main>
   );
 }
+export const products = [
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230208_171632.jpg?updatedAt=1737559254213",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230127_135053.jpg?updatedAt=1737559253578",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230105_140718.jpg?updatedAt=1737559253121",
+  },
+ 
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230127_135003.jpg?updatedAt=1737559252982",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20240227_104656.jpg?updatedAt=1737559252741",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230105_140741(1).jpg?updatedAt=1737559252761",
+  },
+ 
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20240227_160756.jpg?updatedAt=1737559252738",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20221130_101838.jpg?updatedAt=1737559252160",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20221102_123632.jpg?updatedAt=1737559251560",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230920_160338.jpg?updatedAt=1737559250240",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230214_115616.jpg?updatedAt=1737559247575",
+  },
+ 
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230105_140727.jpg?updatedAt=1737559248109",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230127_135031.jpg?updatedAt=1737559247571",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230918_142336.jpg?updatedAt=1737559247552",
+  },
+  {
+    title: "",
+    link: "/",
+    thumbnail:
+      "https://ik.imagekit.io/ocnsmwz76/IMG_20230202_163106.jpg?updatedAt=1737559247454",
+  },
+];
